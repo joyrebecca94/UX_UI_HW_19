af0e57e08cabfcf78c03a34f460cdddd74283b36
joyrebecca94 <137363614+joyrebecca94@users.noreply.github.com>
2023-12-07 22:45:38 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#usda-img").click(function()
+    {
+        $("#usda-card").slideDown("slow");
+    });
+
+    $("#fowl-img").click(function()
+    {
+        $("#fowl-card").slideDown("slow");
+    })
+});
